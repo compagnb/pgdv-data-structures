e56c04a4118068c4c178e9b2859b45c7661d37a4
Jacob Romer <jr@extradat.de>
2015-09-08 15:33:40 -0400

    minor changes to wassign01

--- Assignment #1/wassign01.js
+++ Assignment #1/wassign01.js
@@ -1,9 +1,9 @@
 var request = require('request');
 var fs = require('fs');
 
-request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone=02&borough=M', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    fs.writeFileSync('/home/ubuntu/workspace/data/syllabus.txt', body);
+request('http://www.nyintergroup.org/meetinglist/meetinglist.cfm?zone=02&borough=M', function (err, res, body) {
+  if (!err && res.statusCode == 200) {
+    fs.writeFileSync('./res.txt', body);
   }
-  else {console.error('request failed')}
+  else {console.err('request failed')}
 });
