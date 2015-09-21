93ed459b0dfb5b89cbdf7dfdb0ed5c1bb88f2669
Jacob Romer <jr@extradat.de>
2015-09-21 16:05:03 -0400

    Don't rely on implicit empty final array element

--- Assignment #3/wassign03.js
+++ Assignment #3/wassign03.js
@@ -2,34 +2,37 @@ var fs = require("fs");
 var request = require("request");
 
 var apiKey = process.env.GMAKEY;
-var addresses = fs.readFileSync("res").toString().split("\n");
+var addresses = fs.readFileSync("res")
+  .toString()
+  .split("\n")
+  // Delete empty array elements
+  .filter(function(n){return n});
+  
 var apiUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=";
 var keyString = "&key=" + apiKey;
 var meetingsData = [];
 
-var i = 0; 
-
-function timeOut() {
-  if (i < addresses.length) {
-    var addr = addresses[i]
-      .substr(0, addresses[i].indexOf(","))
-      .replace(/\s\(.*$/, "")
-      .replace(/\s/g, "+");
-    request(apiUrl + addr + "+New+York,+NY" + keyString, function(err, resp, body) {
-      if (err) throw err;
-      meetingsData.push({
-        address: addr.replace(/\+/g, " "),
-        latLong: { 
-          lat: JSON.parse(body).results[0].geometry.location.lat,
-          lng: JSON.parse(body).results[0].geometry.location.lng
-        }
-      });
-
-      i++;
-
-      i < addresses.length - 1 ? setTimeout(timeOut, 110) : console.log(meetingsData);
+function iterateThroughArray(arr, i) {
+  var addr = arr[i]
+    .substr(0, arr[i].indexOf(","))
+    .replace(/\s\(.*$/, "")
+    .replace(/\s/g, "+");
+  request(apiUrl + addr + "+New+York,+NY" + keyString, function(err, resp, body) {
+    if (err) throw err;
+    meetingsData.push({
+      address: addr.replace(/\+/g, " "),
+      latLong: { 
+        lat: JSON.parse(body).results[0].geometry.location.lat,
+        lng: JSON.parse(body).results[0].geometry.location.lng
+      }
     });
-  }
+    console.log("i: " + i + ", arr.length: " + arr.length);
+
+    // if we're at the end of the array, log results.
+    // Otherwise, the function calls itself again after 110ms.
+    i < arr.length - 1 ? setTimeout(iterateThroughArray, 110, arr, i+1) : console.log(meetingsData);
+  });
 }
 
-timeOut();
+console.log(addresses);
+iterateThroughArray(addresses, 0);
