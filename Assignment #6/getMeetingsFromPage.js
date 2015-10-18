0935c400202be9477663042738bff8a6526543cb
Jacob Romer <jmbrjmbr@gmail.com>
2015-10-17 23:29:52 -0400

    typo

--- Assignment #6/getMeetingsFromPage.js
+++ Assignment #6/getMeetingsFromPage.js
@@ -9,7 +9,7 @@ const KEY_STR   = "&key=" + API_KEY;
 
 module.exports = function getMeetingsFromPage(body, cb, cursor) {
   if (!API_KEY) {
-    cursor.red().bold().write("Missing Google API Key!");
+    cursor.red().bold().write("✘ Missing Google API Key!").reset();
     throw "MUST provide GMAKEY";
   }
 
